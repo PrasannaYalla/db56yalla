99032bbc57da7692991b7e0f77c19f77c49e155b
PrasannaYalla <prasannayalla1999@gmail.com>
2021-11-27 20:37:09 -0600

    added code

--- routes/grapes.js
+++ routes/grapes.js
@@ -6,13 +6,23 @@ var router = express.Router();
 router.get('/', grapes_controlers.grapes_view_all_Page);
 
 /* GET detail grapes page */ 
-router.get('/detail', grapes_controlers.grapes_view_one_Page); 
+router.get('/detail', grapes_controlers.grapes_view_one_Page);
+// A little function to check if we have an authorized user and continue on
+// or
+// redirect to login.
+const secured = (req, res, next) => {
+    if (req.user){
+        return next();
+    }
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login");
+    } 
 /* GET create grapes page */ 
-router.get('/create', grapes_controlers.grapes_create_Page);
+router.get('/create',secured, grapes_controlers.grapes_create_Page);
 /* GET create update page */ 
-router.get('/update', grapes_controlers.grapes_update_Page); 
+router.get('/update',secured, grapes_controlers.grapes_update_Page); 
 /* GET create grapes page */ 
-router.get('/delete', grapes_controlers.grapes_delete_Page); 
+router.get('/delete',secured, grapes_controlers.grapes_delete_Page); 
  
 
 
